88b5a53829dc7fdc31005ce98910603e1783b453
sadrajf1377 <150929173+sadrajf1377@users.noreply.github.com>
2025-04-13 17:08:05 -0600

    mes

--- auth_module/views.py
+++ auth_module/views.py
@@ -5,10 +5,16 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.utils.crypto import get_random_string
+from django.utils.decorators import method_decorator
 from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .forms import Login_Form,Signup_Form
 from user_module.models import User_Model
-
+from rest_framework.authtoken.models import Token
+from utils.decorators import check_api_key
+from Task_Manager.settings import SECRET_KEY
 # Create your views here.
 class Login_Singup_Page(View):
     def get(self,request):
@@ -39,6 +45,29 @@ class Login(View):
             return render(request,'Login&Signup.html',context={'login_form':frm,'signup_form':Signup_Form()})
 
 
+@method_decorator(check_api_key,name='dispatch')
+class Login_Api(APIView):
+    http_method_names = ['POST']
+    def post(self,request):
+        data=request.data
+        username_email=data['username_email']
+        password=data['password']
+        try:
+            user=User_Model.objects.get(Q(username=username_email)|Q(email=username_email))
+            if user.check_password(password):
+                tok=Token(user_id=user.id)
+                tok.save()
+                return Response(data={'message':f'your authentication was successful','token':str(tok.key)},status=201)
+            else:
+                return Response(data={'message':'user with such credentials was not found!'},status=404)
+        except Exception as e:
+            if isinstance(e,User_Model.DoesNotExist):
+                return Response(data={'message': 'user with such credentials was not found!'}, status=404)
+            else:
+                return Response(data={'message': 'an unknown error happend!please try again'}, status=500)
+
+
+
 class Signup(View):
     def post(self,request):
         frm=Signup_Form(request.POST)
@@ -52,10 +81,10 @@ class Signup(View):
                     frm.save()
                     return render(request,'Message.html',context={f'User Was Created SuccessFully!credintials are password:{password},username:{frm.cleaned_data.get("username")}',},status=201)
             except:
-                frm.add_error('password_repeat','failed to send email!please try again')
-                return render(request,'Login&Signup.html',context={'login_form':Login_Form(),'signup_form':frm})
+                frm.add_error('password','an error happend,please try again')
+                return render(request,'Login&Signup.html',context={'login_form':Login_Form(),'signup_form':frm},status=500)
         else:
-            return render(request, 'Login&Signup.html', context={'login_form': Login_Form(), 'signup_form': frm})
+            return render(request, 'Login&Signup.html', context={'login_form': Login_Form(), 'signup_form': frm},status=400)
 
 
 class Logout(View):
---
+++ utils/decorators.py
@@ -0,0 +1,14 @@
+from django.http import HttpRequest
+from rest_framework.response import Response
+
+from Task_Manager.settings import SECRET_KEY
+def check_api_key(func):
+    def wrapper(*args,**kwargs):
+        request:HttpRequest=args[0] or kwargs['request']
+        if request.headers.__contains__('api_key') and request.headers.get('api_key')==SECRET_KEY:
+            return func(*args,**kwargs)
+        else:
+            return Response(data={'message':'the given api key was not correct'},status=401)
+    return wrapper
+
+
